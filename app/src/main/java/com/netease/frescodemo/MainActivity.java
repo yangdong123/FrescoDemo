d3ca448357b549bde414bca00909598f4a8e5a4c
bjyangdong <bjyangdong@corp.netease.com>
2017-02-27 13:38:23 +0800

    yd

--- app/src/main/java/com/netease/frescodemo/MainActivity.java
+++ app/src/main/java/com/netease/frescodemo/MainActivity.java
@@ -13,12 +13,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         CustomDraweeView iv_custom = (CustomDraweeView) findViewById(R.id.iv_custom);
+        CustomDraweeView iv_customl = (CustomDraweeView) findViewById(R.id.iv_customl);
         CustomDraweeView iv_roundcustom = (CustomDraweeView) findViewById(R.id.iv_roundcustom);
 
-        iv_custom.loadRectangleByUrl("http://pic6.huitu.com/res/20130116/84481_20130116142820494200_1.jpg");
+        iv_custom.loadPicByUrl("http://pic6.huitu.com/res/20130116/84481_20130116142820494200_1.jpg");
 
-//        iv_custom.loadPicByUrl("http://pic6.huitu.com/res/20130116/84481_20130116142820494200_1.jpg");
-//
+        iv_customl.loadRectangleByUrl("http://pic6.huitu.com/res/20130116/84481_20130116142820494200_1.jpg");
+
         iv_roundcustom.loadRoundPicByUrl("http://pic6.huitu.com/res/20130116/84481_20130116142820494200_1.jpg");
         iv_roundcustom.setDraweeViewBorder(8,R.color.colorPrimaryDark);
 
